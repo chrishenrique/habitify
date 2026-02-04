866f3e05554a205d9423ed191622130cdde38815
Christiano Costa <chrishenrique16@hotmail.com>
2026-02-04 16:45:49 -0300

    WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,19 +7,19 @@
         
         <div class="grid auto-rows-min gap-4 md:grid-cols-12">
             <div class="relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 md:col-span-10 md:row-span-20">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                <livewire:habits class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
             <div class="relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 md:col-span-2 md:row-span-20">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                <livewire:habit-progress class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
         </div>
         
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <livewire:progress class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
         
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <livewire:chart class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
 </x-layouts::app>
